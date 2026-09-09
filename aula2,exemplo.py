eb7d889a14eda7382a2e432ab1d5d5ef64ff8e75
gabriel <gabriel.candancan@aluno.impacta.edu.br>
2026-09-08 21:31:02 -0300

    Atualiza aula2 exemplo

--- aula2,exemplo.py
+++ aula2,exemplo.py
@@ -1,5 +1,5 @@
 d=int(input("qual é a distancia da sua viagem: "))
-ck=int(input("qunato de gasolina seu carro gasta por km: "))
+ck=int(input("quanto de gasolina seu carro gasta por km: "))
 pc=int(input("quanto custa o combustivel: "))
 
 custo=d*ck*pc
